f7bec4dc1964c8816ae34351dac7552ea6c28167
Mike Dent <mikedent85@gmail.com>
2020-12-31 16:49:29 -0500

    deployment again

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const api = {
-  API_ENDPOINT: 'https://desolate-reaches-40844.herokuapp.com/',
+  API_ENDPOINT: 'https://desolate-reaches-40844.herokuapp.com',
   API_KEY: process.env.REACT_APP_API_KEY  
 }
 
